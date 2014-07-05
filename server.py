db4b10c3868f9fccd42e04ad02099d3d66547369
Ruslan Gustomyasov <rusik@4ege.ru>
2014-07-05 12:42:06 +0400

    /info added

--- server.py
+++ server.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python3
 
+import os
+
 import tornado.ioloop
 import tornado.web
 
-DownloadDir = '/home/rusik/PycharmProjects/blah/data/'
+
+Dir = '/home/rusik/PycharmProjects/blah/data/'
 
 
 class DownloadHandler(tornado.web.RequestHandler):
     def get(self, filename):
-        with open(DownloadDir + filename, 'rb') as f:
+        with open(Dir + filename, 'rb') as f:
             self.write(f.read())
             f.close()
 
 
 class UploadHandler(tornado.web.RequestHandler):
     def post(self, filename):
-        with open(filename, 'wb') as f:
+        with open(Dir + filename, 'wb') as f:
             f.write(self.request.body)
+
+
+class InfoHandler(tornado.web.RequestHandler):
+    def get(self, filename):
+        if os.path.isfile(Dir + filename):
+            self.write("1")
+        else:
+            self.write("0")
 
 
 application = tornado.web.Application([
     (r"/download/(.*)", DownloadHandler),
     (r"/upload/(.*)", UploadHandler),
+    (r"/info/(.*)", InfoHandler)
 ])
 
 if __name__ == '__main__':
